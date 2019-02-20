a6fd666aa81cac7ede14d3d1257a85a509c1fb60
Bravosip <xzh199111@outlook.com>
2019-02-20 23:13:40 +0800

    2/20 commit add druid datasource

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,8 +5,6 @@ import com.example.demo.dto.User;
 import com.example.demo.service.UserService;
 import com.example.demo.log.util.IdMaker;
 import lombok.extern.log4j.Log4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
